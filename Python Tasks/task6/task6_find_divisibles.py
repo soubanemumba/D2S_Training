e2658572c28410a7eb0f8bc9e06556f08ee9cc54
soubanemumba <145340546+soubanemumba@users.noreply.github.com>
2023-09-18 18:35:01 +0500

    Add files via upload

---
+++ Python Tasks/task6/task6_find_divisibles.py
@@ -0,0 +1,33 @@
+import time
+
+def find_divisibles(in_range,divisor):
+    
+    #fetching the time-1
+    start = time.time()
+    print("find_divisibles called with range", in_range, "and divisor",divisor,"\n")
+    
+    #list variable for storing the number which are divisible by the divisor
+    numbers_divisible_by_divisor= []
+    
+    #iterative loop to check dividend and appending the
+    #required number in the list
+    for num in range (1, in_range+1):
+        if num % divisor == 0:
+           numbers_divisible_by_divisor.append(num)
+
+    print(numbers_divisible_by_divisor)
+    # fetching the time-2, to calculate the difference
+    end = time.time()
+    #finding the elapsed time by difference
+    total_time_elapsed = end - start
+    print("find_divisibles called with range" , in_range , "and divisor" , divisor, "and it took" , total_time_elapsed , "second \n")
+    return(numbers_divisible_by_divisor)
+
+if __name__ == "__main__":
+    # function calling
+    print("\n")
+    find_divisibles(5080000,34113)
+    print("\n")
+    find_divisibles(100052,3210)
+    print("\n")
+    find_divisibles(500,3)
